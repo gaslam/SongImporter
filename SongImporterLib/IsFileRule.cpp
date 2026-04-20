2bb40a5da726268ada80c262dc3be070cd671fad
gaslam <44768183+gaslam@users.noreply.github.com>
2026-04-20 16:07:14 +0200

    Add url check

--- SongImporterLib/IsFileRule.cpp
+++ SongImporterLib/IsFileRule.cpp
@@ -1,22 +1,33 @@
 #include "IsFileRule.h"
 #include <QFileInfo>
+#include <QUrl>
 
-IsFileRule::IsFileRule(QObject *parent)
+IsFileRule::IsFileRule(QObject* parent)
 	: Rule(parent)
 {}
 
 IsFileRule::~IsFileRule()
 {}
 
-OperationResult IsFileRule::validate(const QVariant & value) const
+OperationResult IsFileRule::validate(const QVariant& value) const
 {
 
 	QFileInfo fileInfo{ };
 	switch (value.typeId())
 	{
-		case QMetaType::QString:
-		case QMetaType::QUrl:
-			fileInfo = QFileInfo{ value.toString() };
+	case QMetaType::QUrl:
+	{
+		const QUrl url = value.toUrl();
+		if (!url.isLocalFile())
+		{
+			return OperationResult::fail("The path does not point to a local file");
+		}
+		fileInfo = QFileInfo{ url.toLocalFile() };
+	}
+	break;
+
+	case QMetaType::QString:
+		fileInfo = QFileInfo{ value.toString() };
 		break;
 	default:
 		return OperationResult::fail("Cannot read any text from the input field");
