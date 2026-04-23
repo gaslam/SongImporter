0072840bb64f02f6a651ca349296b57f84191b6b
gaslam <44768183+gaslam@users.noreply.github.com>
2026-04-23 18:31:01 +0200

    Add Supported audio formats

--- SongImporterLib/SoftwareUtils.h
+++ SongImporterLib/SoftwareUtils.h
@@ -5,6 +5,16 @@
 class SoftwareUtils  : public QObject
 {
 	Q_OBJECT
+	enum SupportedAudioFormats {
+		MP3,
+		AIFF,
+		WAV,
+		FLAC,
+		ALAC,
+		AAC,
+
+	};
+	Q_ENUM(SupportedAudioFormats)
 
 public:
 	SoftwareUtils(QObject *parent);
