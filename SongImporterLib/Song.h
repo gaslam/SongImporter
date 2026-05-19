d53ae9932b403ef85857907f0596b19e9fde1f4e
gaslam <44768183+gaslam@users.noreply.github.com>
2026-05-19 18:11:40 +0200

    Add variables to struct

--- SongImporterLib/Song.h
+++ SongImporterLib/Song.h
@@ -3,6 +3,15 @@
 
 struct Song {
 	Q_GADGET
+
+	const unsigned int year;
+	const QString title;
+	const QString album;
+	const QUrl albumCover;
+	const QString songArtists;
+	const QString albumArtists;
+	const QString filename;
+	const unsigned int bpm;
 };
 
 Q_DECLARE_METATYPE(Song)
