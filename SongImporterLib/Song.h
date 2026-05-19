8f60c2a12163926b45f22fa0c65177b7c927964b
gaslam <44768183+gaslam@users.noreply.github.com>
2026-05-19 18:24:28 +0200

    Intialize in constructor

--- SongImporterLib/Song.h
+++ SongImporterLib/Song.h
@@ -4,11 +4,29 @@
 struct Song {
 	Q_GADGET
 
+public:
+	Song(const unsigned int& songYear,
+		const QString& songTitle,
+		const QString& songAlbum,
+		const QString& songArtists,
+		const QString& songAlbumArtists,
+		const QString& songFile,
+		const unsigned int songBpm):
+		year{songYear},
+		album{songAlbum},
+		title{songTitle},
+		artists{songArtists},
+		albumArtists{songAlbumArtists},
+		filename{songFile},
+		bpm{songBpm}
+	{
+
+	}
+
 	const unsigned int year;
 	const QString title;
 	const QString album;
-	const QUrl albumCover;
-	const QString songArtists;
+	const QString artists;
 	const QString albumArtists;
 	const QString filename;
 	const unsigned int bpm;
